8f8f21e58c7af08b1efadd65c224214664540833
ferreusveritas <ferreusveritas@gmail.com>
2018-06-13 14:17:19 -0400

    Break everything, pretend it didn't happen

--- src/main/java/com/ferreusveritas/stargarden/StarGarden.java
+++ src/main/java/com/ferreusveritas/stargarden/StarGarden.java
@@ -3,6 +3,7 @@ package com.ferreusveritas.stargarden;
 import com.ferreusveritas.mcf.FeatureableMod;
 import com.ferreusveritas.stargarden.features.Banners;
 import com.ferreusveritas.stargarden.features.ComputerCraft;
+import com.ferreusveritas.stargarden.features.Logo;
 import com.ferreusveritas.stargarden.features.ProjectRed;
 import com.ferreusveritas.stargarden.features.Thermal;
 import com.ferreusveritas.stargarden.features.Vanilla;
@@ -10,6 +11,10 @@ import com.ferreusveritas.stargarden.proxy.CommonProxy;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 /**
 * <p><pre><tt><b>
@@ -46,7 +51,19 @@ public class StarGarden extends FeatureableMod {
 	
 	@Override
 	protected void setupFeatures() {
-		addFeatures(new Vanilla(), new Thermal(), new ProjectRed(), new ComputerCraft(), new Banners());
+		addFeatures(new Vanilla(), new Thermal(), new ProjectRed(), new ComputerCraft(), new Banners(), new Logo());
 	}
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) { super.preInit(event); }
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) { super.init(event); }
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) { super.postInit(event); }
+	
+	@Mod.EventHandler
+	public void onFMLLoadComplete(FMLLoadCompleteEvent event) { super.onFMLLoadComplete(event); }
 	
 }
